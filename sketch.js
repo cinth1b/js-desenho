9085cd8a7931de0535d217fb34b333889a041008
cinth1b <00001115061355SP@al.educacao.sp.gov.br>
2024-08-15 13:22:29 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("red");
+  }
+  
+  function draw() {
+    stroke ("green");
+    fill ("blue");
+  if (mouseIsPressed) {
+    rect(mouseX.mouseX. 20 . 45)
+  }
